1175576c70d6cc05ad294a8673a2b508ad809ae8
PaulHnsv <65260913+PaulHnsv@users.noreply.github.com>
2021-05-02 19:32:24 -0300

    feat: refatoramento da logica do server

--- src/config/ServerConfig.java
+++ src/config/ServerConfig.java
@@ -1,27 +1,111 @@
 package config;
 
-import java.io.InputStream;
-import java.util.Scanner;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 import main.ServidorTCPBasico;
 
 public class ServerConfig implements Runnable {
 
-	private InputStream cliente;
-	private ServidorTCPBasico servidor;
+	private List<ServerConfig> clientes = new ArrayList<ServerConfig>();;
 
-	public ServerConfig(InputStream cliente, ServidorTCPBasico servidor) {
-		this.cliente = cliente;
-		this.servidor = servidor;
+	private Socket cliente;
+	private DataInputStream in;
+	private DataOutputStream out;
+	private String boasVindas;
+
+	public ServerConfig(Socket cliente, ServidorTCPBasico servidor) {
+		// indica que determinado client entrou no chat
+		this.boasVindas = "Cliente " + cliente.getInetAddress().getHostAddress() + " entrou no chat." + "\n";
+
+		// lógica do envio e recebimento das mensagens por parte do server
+		try {
+			this.cliente = cliente;
+			clientes.add(this);
+
+			in = new DataInputStream(new BufferedInputStream(cliente.getInputStream()));
+			out = new DataOutputStream(new BufferedOutputStream(cliente.getOutputStream()));
+
+			distribuiMensagem(this.boasVindas);
+
+			// synchronized (out) {
+			// out.writeUTF("Bem vindo ao chat " + cliente.getInetAddress().getHostAddress()
+			// + "\n");
+			// }
+			// out.flush();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
 	}
 
 	public void run() {
-		// quando chegar uma msg, distribui pra todos
-		Scanner s = new Scanner(this.cliente);
-		while (s.hasNextLine()) {
-			servidor.distribuiMensagem(s.nextLine(), servidor.cliente);
+		// lógica do chat para desligamento e manutenção de uma thread
+		try {
+			try {
+				boolean sair = false;
+				while (!sair) {
+					String texto = in.readUTF();
+					if ("SAIR".equals(texto)) {
+						sair = true;
+						clientes.remove(this);
+						distribuiMensagem("Cliente " + cliente.getInetAddress().getHostAddress() + " saiu do chat.");
+					} else {
+						distribuiMensagem(texto);
+					}
+				}
+			} finally {
+				if (!this.cliente.isClosed()) {
+					this.cliente.close();
+				}
+			}
+		} catch (IOException ex) {
+			ex.printStackTrace();
+			System.out.println("Não conseguiu comunicar com o cliente.");
 		}
-		s.close();
+	}
+
+	// método que formata uma mensagem
+	public void distribuiMensagem(String msg) {
+		LocalDateTime dataAgora = LocalDateTime.now();
+
+		// // formatar a data
+		// DateTimeFormatter formatterData = DateTimeFormatter.ofPattern("dd/MM/uuuu");
+		// String dataFormatada = formatterData.format(dataAgora);
+
+		// formatar a hora
+		DateTimeFormatter formatterHora = DateTimeFormatter.ofPattern("HH:mm:ss");
+		String horaFormatada = formatterHora.format(dataAgora);
+
+		System.out.println("Enviar msg: " + msg);
+		synchronized (clientes) {
+			for (ServerConfig cliente : this.clientes) {
+
+				try {
+					synchronized (cliente.out) {
+						if (!msg.equals(boasVindas)) {
+							cliente.out.writeUTF(msg + " enviado às: " + horaFormatada + " por: "
+									+ this.cliente.getInetAddress().getHostAddress() + "\n");
+						} else {
+							cliente.out.writeUTF(msg);
+						}
+					}
+					cliente.out.flush();
+
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+
+			}
+		}
+
 	}
 
 }
--- src/main/ServidorTCPBasico.java
+++ src/main/ServidorTCPBasico.java
@@ -1,77 +1,58 @@
 package main;
 
 import java.io.IOException;
-import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.sql.Date;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
+import java.net.UnknownHostException;
 
 import config.ServerConfig;
 
 public class ServidorTCPBasico {
+
+	private int porta;
+	public Socket cliente;
+
 	public static void main(String[] args) throws IOException {
-		// inicia o servidor
+
+		// inicia o server
 		new ServidorTCPBasico(12345).executa();
 	}
 
-	private int porta;
-	private List<PrintStream> clientes;
-	private List<Socket> clientesLogados;
-	public Socket cliente;
-
 	public ServidorTCPBasico(int porta) {
 		this.porta = porta;
-		this.clientes = new ArrayList<PrintStream>();
-		this.clientesLogados = new ArrayList<Socket>();
+
 	}
 
 	public void executa() throws IOException {
 		ServerSocket servidor = new ServerSocket(this.porta);
 		System.out.println("Porta 12345 aberta!");
 
-		while (true) {
-			// aceita um cliente
-			this.cliente = servidor.accept();
-			System.out.println("Nova conexão com o cliente " + cliente.getInetAddress().getHostAddress());
-			this.clientesLogados.add(cliente);
+		try {
+			while (true) {
+				// aceita um cliente
+				this.cliente = servidor.accept();
+				System.out.println("Nova conexão com o cliente " + cliente.getInetAddress().getHostAddress());
 
-			// adiciona saida do cliente à lista
-			PrintStream ps = new PrintStream(cliente.getOutputStream());
-			this.clientes.add(ps);
-
-			// cria tratador de cliente numa nova thread
-			ServerConfig tc = new ServerConfig(cliente.getInputStream(), this);
-			new Thread(tc).start();
-		}
-
-	}
-
-	public void distribuiMensagem(String msg, Socket clienteQueEnviou) {
-		LocalDateTime dataAgora = LocalDateTime.now();
-
-		// formatar a data
-		DateTimeFormatter formatterData = DateTimeFormatter.ofPattern("dd/MM/uuuu");
-		String dataFormatada = formatterData.format(dataAgora);
-
-		// formatar a hora
-		DateTimeFormatter formatterHora = DateTimeFormatter.ofPattern("HH:mm:ss");
-		String horaFormatada = formatterHora.format(dataAgora);
-
-		// envia msg para todo mundo menos para o remetente
-		for (Socket cliente : this.clientesLogados) {
-			if (!cliente.equals(clienteQueEnviou)) {
-				try {
-					PrintStream mensagem = new PrintStream(cliente.getOutputStream());
-					mensagem.println(msg + " enviado às: " + horaFormatada + " do dia " + dataFormatada);
-				} catch (IOException e) {
-					e.printStackTrace();
+				// cria tratador de cliente numa nova thread
+				ServerConfig server = new ServerConfig(cliente, this);
+				new Thread(server).start();
+			}
+		} catch (UnknownHostException ex) {
+			ex.printStackTrace();
+			System.out.println("Não encontrou o host servidor.");
+		} catch (IOException ex) {
+			ex.printStackTrace();
+			System.out.println("Não conseguiu abrir conexão com o host.");
+		} finally {
+			try {
+				if (!servidor.isClosed()) {
+					servidor.close();
 				}
-
+			} catch (IOException ex) {
+				ex.printStackTrace();
+				System.out.println("Erro ao fechar a conexão do socket servidor.");
 			}
 		}
 	}
+
 }
